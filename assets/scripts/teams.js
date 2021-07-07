c70bad8e114f1e081bd4e61095a541b265e7398b
Gabriel Pereira <gabrielspereira2604@gmail.com>
2021-07-07 17:30:52 -0300

    Country team and request js

--- assets/scripts/teams.js
+++ assets/scripts/teams.js
@@ -1,14 +1,41 @@
-const colorThief = new ColorThief();
-const img = new Image();
+const url = new URLSearchParams(window.location.search);
+const id = url.get('id');
 
-img.addEventListener('load', function() {
-  var color = colorThief.getColor(img);
-  console.log(color);
-  $(".banner").css("background-color", "rgb(" + color[0] + ", " + color[1] + ", " + color[2] + ")");
+const settings = {
+	"async": true,
+	"crossDomain": true,
+	"url": "https://api-football-v1.p.rapidapi.com/v3/teams?id=" + id,
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-key": "5f24625205msh7220a67930e7cd0p1a6bb8jsn78ee6010eedc",
+		"x-rapidapi-host": "api-football-v1.p.rapidapi.com"
+	}
+};
+
+$.ajax(settings).done(function (response) {
+  const country = response["response"]["0"]["team"]["country"];
+  $("#nome").html(response["response"]["0"]["team"]["name"]);
+  $("#logo").attr("src", response["response"]["0"]["team"]["logo"]);
+
+  const colorThief = new ColorThief();
+  const img = new Image();
+
+  img.addEventListener('load', function() {
+    var color = colorThief.getColor(img);
+    $(".banner").css("background-color", "rgb(" + color[0] + ", " + color[1] + ", " + color[2] + ")");
+  });
+
+  let imageURL = 'https://media.api-sports.io/football/teams/' + id + '.png';
+  let googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
+
+  img.crossOrigin = 'Anonymous';
+  img.src = googleProxyURL + encodeURIComponent(imageURL);
+
+  $.ajax({
+    url: 'https://restcountries.eu/rest/v2/name/' + country,
+    success: function(response) {
+      $("#nomeCountry").html(response["0"]["translations"]["br"]);
+      $("#logoCountry").attr("src", response["0"]["flag"]);
+    },
+  });
 });
-
-let imageURL = 'https://media.api-sports.io/football/teams/131.png';
-let googleProxyURL = 'https://images1-focus-opensocial.googleusercontent.com/gadgets/proxy?container=focus&refresh=2592000&url=';
-
-img.crossOrigin = 'Anonymous';
-img.src = googleProxyURL + encodeURIComponent(imageURL);
--- teams.php
+++ teams.php
@@ -1,3 +1,8 @@
+<?php
+  if(!empty($_GET)){
+    $id = $_GET['id'];
+    if($id > 0){
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,35 +14,35 @@
   <link rel="stylesheet" href="assets/styles/teams.css"> 
 </head>
 <body>
-
   <?php
-    $curl = curl_init();
+    // $curl = curl_init();
 
-    curl_setopt_array($curl, [
-      CURLOPT_URL => "https://api-football-v1.p.rapidapi.com/v3/teams?name=Corinthians",
-      CURLOPT_CUSTOMREQUEST => "GET",
-      CURLOPT_HTTPHEADER => [
-        "x-rapidapi-host: api-football-v1.p.rapidapi.com",
-        "x-rapidapi-key: 5f24625205msh7220a67930e7cd0p1a6bb8jsn78ee6010eedc"
-      ],
-    ]);
+    // curl_setopt_array($curl, [
+    //   CURLOPT_URL => "https://api-football-v1.p.rapidapi.com/v3/teams?id=".$id,
+    //   CURLOPT_CUSTOMREQUEST => "GET",
+    //   CURLOPT_HTTPHEADER => [
+    //     "x-rapidapi-host: api-football-v1.p.rapidapi.com",
+    //     "x-rapidapi-key: 5f24625205msh7220a67930e7cd0p1a6bb8jsn78ee6010eedc"
+    //   ],
+    // ]);
 
-    curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
-    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+    // curl_setopt($curl, CURLOPT_FOLLOWLOCATION, TRUE);
+    // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+    // curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
     
-    $response = curl_exec($curl);
-    $err = curl_error($curl);
+    // $response = curl_exec($curl);
+    // $err = curl_error($curl);
     
-    $datasearch = json_decode($response, true);
-    $logo = $datasearch["response"]["0"]["team"]["logo"];
-    $nome = $datasearch["parameters"]["name"];
-    curl_close($curl);
+    // $datasearch = json_decode($response, true);
+    // $logo = $datasearch["response"]["0"]["team"]["logo"];
+    // $nome = $datasearch["response"]["0"]["team"]["name"];
+    // curl_close($curl);
     
-    if ($err) {
-      echo "cURL Error #:" . $err;
-    } 
+    // if ($err) {
+    //   echo "cURL Error #:" . $err;
+    // } 
   ?>
+  
   <header id="main-header">
     <div class="content">
       <div class="side">
@@ -47,8 +52,14 @@
   </header>
 
   <div class="banner">
-    <img class="logo" id="logo" src="<?php echo $logo; ?>" alt="Logo do <?php echo $nome; ?>">
-    <h1><?php echo $nome; ?></h1>
+    <div class="banner-row">
+      <img class="logo" id="logo">
+      <h1 id="nome"></h1>
+    </div>
+    <div class="banner-row">
+      <img class="logoCountry" id="logoCountry">
+      <h6 id="nomeCountry"></h6>
+    </div>
   </div>
 
   <script src="https://cdnjs.cloudflare.com/ajax/libs/color-thief/2.3.0/color-thief.umd.js"></script>
@@ -59,3 +70,7 @@
   <script src="assets/scripts/teams.js"></script>
 </body>
 </html>
+<?php
+    }
+  }
+?>
